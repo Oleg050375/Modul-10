d195b3e16068fffb74474f4eb481f01f15a95e69
Oleg050375 <ov_varganov@mail.ru>
2024-10-25 09:01:31 +0500

    Add files via upload

--- bank.py
+++ bank.py
@@ -1,38 +1,45 @@
 import time
 import threading
+import random
+from threading import Lock
 
-#lock1 = Lock()
+lock1 = Lock()
 
 class Bank:
     def __init__(self, balance):
         self.balance = balance
     def deposit(self):
-        for i in range(10):
-            self.balance = self.balance + 10
-            print(f'Пополнение: {'10'}. Баланс: {str(self.balance)}')
-            time.sleep(0.1)
+        for i in range(100):
+            a = random.randint(50,500)
+            if self.balance >= 500 and lock1.locked():
+                lock1.release()  # закрытие окна блокировки
+            else:
+                pass
+            self.balance = self.balance + a
+            print(f'Пополнение: {str(a)}. Баланс: {str(self.balance)}')
+            time.sleep(0.001)
     def take(self):
-        for i in range(10):
-            a = 20
+        for i in range(100):
+            a = random.randint(50,500)
             print(f'Запрос на: {str(a)}.')
             if a <= self.balance:
                 self.balance = self.balance - a
-                print(f'Снятие: {str(a)}')
-                time.sleep(0.1)
+                print(f'Снятие: {str(a)}. Баланс: {str(self.balance)}')
+                time.sleep(0.001)
             else:
                 print('Запрос отклонён, недостаточно средств.')
-                time.sleep(0.1)
+                lock1.acquire()
 
 # TEST ----------------------------------------------------------------------------------------------------------------
 
-bnk = Bank(100)
+bnk = Bank(0)
 
-dep = threading.Thread(target=bnk.deposit(), args=())
-tak = threading.Thread(target=bnk.take(), args=())
+dep = threading.Thread(target=bnk.deposit, args=())
+tak = threading.Thread(target=bnk.take, args=())
 
-#dep.start()
-#tak.start()
-#dep.join()
-#tak.join()
+dep.start()
+tak.start()
+dep.join()
+tak.join()
 
-#bnk.take()
+print(f'Итоговый балланс: {bnk.balance}')
